70006dcdb6436aec1e27724ddec165e3550f2325
niadzgnreal <niadzgn@gmail.com>
2026-05-04 19:34:51 +0700

    Update [slug].js

--- functions/post/[slug].js
+++ functions/post/[slug].js
@@ -100,7 +100,7 @@ export async function onRequest(context) {
       ${breadcrumb}
 
       <article class="post">
-        <img loading="lazy" src="/og/${slug}" alt="${post.title}" width="400" height="179">
+        <img loading="lazy" src="/og/${slug}" alt="${post.title}" width="400" height="210">
         <h1>${post.title}</h1>
 
         <p>⏱ ${readingTime} min read</p>
