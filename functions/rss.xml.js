f2d7f4201bc0c75094b83339df31e771933e0855
niadzgnreal <niadzgn@gmail.com>
2026-05-04 17:43:43 +0700

    Update rss.xml.js

--- functions/rss.xml.js
+++ functions/rss.xml.js
@@ -1,22 +1,36 @@
 import { getPosts } from "../lib/api";
+import { SITE, sanitizeSlug } from "../lib/config";
 
 export async function onRequest() {
   const posts = await getPosts();
 
   const items = posts.slice(0, 20).map(p => `
 <item>
-<title>${p.title}</title>
-<link>https://niadzgn.pages.dev/post/${p.slug}</link>
+<title>${escapeXML(p.title)}</title>
+<link>${SITE.domain}/post/${sanitizeSlug(p.slug)}</link>
 </item>`).join("");
 
   return new Response(`<?xml version="1.0"?>
 <rss version="2.0">
 <channel>
-<title>Auto Blog</title>
-<link>https://niadzgn.pages.dev</link>
+<title>${SITE.name}</title>
+<link>${SITE.domain}</link>
 ${items}
 </channel>
 </rss>`, {
     headers: { "content-type": "application/xml" }
   });
+}
+
+// ======================
+// ESCAPE XML (WAJIB BIAR VALID)
+// ======================
+function escapeXML(str = "") {
+  return String(str).replace(/[<>&'"]/g, c => ({
+    "<": "&lt;",
+    ">": "&gt;",
+    "&": "&amp;",
+    "'": "&apos;",
+    '"': "&quot;"
+  }[c]));
 }
