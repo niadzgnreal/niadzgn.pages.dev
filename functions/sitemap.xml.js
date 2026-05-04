52393cdf4194edf5ad3eb816ba073ad277c1d266
niadzgnreal <niadzgn@gmail.com>
2026-05-04 17:41:32 +0700

    Update sitemap.xml.js

--- functions/sitemap.xml.js
+++ functions/sitemap.xml.js
@@ -1,11 +1,12 @@
 import { getPosts } from "../lib/api";
+import { SITE, sanitizeSlug } from "../lib/config";
 
 export async function onRequest() {
   const posts = await getPosts();
 
   const urls = posts.map(p => `
 <url>
-  <loc>https://niadzgn.pages.dev/post/${p.slug}</loc>
+  <loc>${SITE.domain}/post/${sanitizeSlug(p.slug)}</loc>
 </url>`).join("");
 
   return new Response(`<?xml version="1.0"?>
