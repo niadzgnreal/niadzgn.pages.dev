dbb80994307dd62c0c06c8075845b458b8afdad1
niadzgnreal <niadzgn@gmail.com>
2026-05-04 16:02:38 +0700

    Update [slug].js

--- functions/og/[slug].js
+++ functions/og/[slug].js
@@ -73,7 +73,7 @@ export async function onRequest(context) {
   </text>
 
   <!-- Title -->
-  <text x="130" y="260" fill="white" font-size="60" font-weight="bold" font-family="sans-serif">
+  <text x="200" y="260" fill="white" font-size="60" font-weight="bold" font-family="sans-serif">
     ${wrapText(title, 26)}
   </text>
 
